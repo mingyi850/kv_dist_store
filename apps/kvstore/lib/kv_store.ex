b3958508b7b1cf1922e080aa2f0ad9f87a2aeb7b
Johannson Lim <mingyi850@gmail.com>
2024-04-29 18:09:52 -0400

    update vector clock during put request

--- apps/kvstore/lib/kv_store.ex
+++ apps/kvstore/lib/kv_store.ex
@@ -214,6 +214,7 @@ alias KvStore.GetResponse
   @spec handle_put_request(%KvStore{}, %KvStore.PutRequest{}) :: %KvStore{}
   def handle_put_request(state, request) do
     updated_context = get_updated_context(state, request)
+    state = %{state | clock: updated_context.vector_clock}
     preference_list = get_preference_list(request.key, state, state.replication_factor)
     internal_request = KvStore.InternalPutRequest.new(request, updated_context, state.counter)
     timer = Emulation.timer(state.internal_timeout, {:timeout, state.counter, 1})
@@ -295,6 +296,7 @@ alias KvStore.GetResponse
   end
 
   #Combines current clock with node's clock to get latest clock
+  @spec get_updated_context(%KvStore{}, %KvStore.PutRequest{}) :: %KvStore.Context{}
   defp get_updated_context(state, request) do
     if request.contexts != [] do
       KvStore.Context.new(combine_vector_clocks([state.clock | Enum.map(request.contexts, fn ctx -> ctx.vector_clock end)]))
